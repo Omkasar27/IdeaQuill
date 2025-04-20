2aa3f3c2cab8bc470b0247010db5ccab506ee7d8
Omkasar27 <omkasar80@gmail.com>
2025-04-20 13:00:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,15 +6,13 @@ const userApp = require("./APIs/userApi");
 const authorApp = require("./APIs/authApi");
 const adminApp = require("./APIs/adminApi");
 const cors=require('cors')
-app.use(cors(
-      {
-        origin: ['https://ideaquill-1.onrender.com', 'https://ideaquilll.netlify.app/'], // Replace with your frontend URL
-        
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true
-    }
-))
+app.use(cors({
+  origin: ['https://ideaquill-1.onrender.com', 'https://ideaquilll.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 
 const port = process.env.PORT || 3000;
 
